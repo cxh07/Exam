22541817def32074b62ad64a420e8ea90cb26b99
cxh <502623564@qq.com>
2021-01-25 01:18:26 +0800

    不要的

---
+++ exam/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+     <router-view :key="key"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ exam/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>123</h1>
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return{
+      msg:"开始你登录界面的书写吧"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
